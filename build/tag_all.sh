2c91713c7be50e2fe55806ec0ed0cde87721d180
Anant <anant.asty@gmail.com>
2014-10-23 11:12:23 -0600

    Addded log file to data files

--- build/tag_all.sh
+++ build/tag_all.sh
@@ -5,13 +5,14 @@ if [[ "$USER" != "root" ]]; then
     exit 1
 fi
 
-image_list=("spark-master:0.9.0" "spark-worker:0.9.0" "spark-shell:0.9.0" "shark-master:0.8.0" "shark-worker:0.8.0" "shark-shell:0.8.0" )
+image_list=("spark-master:1.1.0" "spark-worker:1.1.0" "spark-shell:1.1.0" "spark-shell-py:1.1.0" "shark-master:0.8.0" "shark-worker:0.8.0" "shark-shell:0.8.0" )
 
-IMAGE_PREFIX="amplab/"
+IMAGE_PREFIX="anantasty/"
 
 # NOTE: the order matters but this is the right one
 for i in ${image_list[@]}; do
 	image=$(echo $i | awk -F ":" '{print $1}')
-        echo docker tag ${IMAGE_PREFIX}${i} ${IMAGE_PREFIX}${image}:latest
-	docker tag ${IMAGE_PREFIX}${i} ${IMAGE_PREFIX}${image}:latest
+        version=$(echo $i | awk -F ":" '{print $2}')
+        echo docker tag ${i} ${IMAGE_PREFIX}${image}
+	docker tag ${i} ${IMAGE_PREFIX}${image}:${version}
 done
